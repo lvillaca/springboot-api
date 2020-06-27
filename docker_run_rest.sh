0a65bfa5b68c9fae780c64c80992c44f59990968
lvilla <lvillaca@users.noreply.github.com>
2020-06-27 12:37:03 -0300

    Indicate params

--- docker_run_rest.sh
+++ docker_run_rest.sh
@@ -1,9 +1,6 @@
-## RESULT=`curl -v -k --data "grant_type=client_credentials&client_id=<client_id>&client_secret=<client_secret>" https://idp.com.br/auth/realms/API/protocol/openid-connect/token`
+# set required params (client id, secret, idp url, realm, sp url - locolhost and port for testing)
 
-
-
-RESULT=`curl  -k --data "grant_type=client_credentials&client_id=oidcapi&client_secret=017c2137-153a-4cab-bfc3-dcfca356bb82" https://oidc.transpetro.com.br/auth/realms/API/protocol/openid-connect/token`
-
+RESULT=`curl -v -k --data "grant_type=client_credentials&client_id=<client_id>&client_secret=<client_secret>" https://<idp url>/auth/realms/<realm>/protocol/openid-connect/token`
 
 echo $RESULT | python -m json.tool
 TOKEN=`echo $RESULT | sed 's/.*access_token":"\([^"]*\).*/\1/'`
@@ -12,5 +9,4 @@ echo $TOKEN
 
 echo attempting to pull from token..............................................................................
 
- curl  -k  -H "Authorization: Bearer $TOKEN"      https://localhost:8974/api/funcionario/LHNV
-## curl  -k -X GET -H "Authorization: Bearer $TOKEN"   -H "Accept: application/json"   https://localhost:8974/funcionario/LHNV
+curl  -k  -H "Authorization: Bearer $TOKEN"      https://<sp url>/api/funcionario/LHNV
